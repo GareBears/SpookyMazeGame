6571bc28494f44dcf108e2df2f019f63ab1f8c36
Garrett <73368079+GareBears@users.noreply.github.com>
2023-09-29 12:11:04 -0400

    Started Player Settings
    -Added Cam settings
    -Started Player movement
    -Started flashlight settings

---
+++ SpookyMazeGame/Assets/Scripts/PlayerStuff/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
