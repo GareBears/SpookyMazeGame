ac15a633481bba6ee0333b7378441615d239c12f
Garrett <73368079+GareBears@users.noreply.github.com>
2023-10-01 13:43:54 -0400

    Added Movement
    -Added Player movement

--- SpookyMazeGame/Assets/Scripts/PlayerStuff/PlayerController.cs
+++ SpookyMazeGame/Assets/Scripts/PlayerStuff/PlayerController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
+    [Header("Ground Check")]
     public float moveSpeed;
+    public float playerHeight;
+    public float groundDrag;
+    public LayerMask ground;
+    bool grounded;
 
     public Transform orientation;
 
@@ -16,15 +23,59 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody rb;
 
-    // Start is called before the first frame update
+    /////////////////////////////////////////////////////////////////////////////////
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    /////////////////////////////////////////////////////////////////////////////////
+
+    private void Update()
     {
-        
+        MyInput();
+        SpeedControl();
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+        if(grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
